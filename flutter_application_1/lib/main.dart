9bfbdc53b2aabf781f1a2a27bcddbd4b43f3e9c9
ณัฐดนัย <ritttt@MacBook-Air-3.local>
2024-01-08 21:15:55 +0700

    up

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/src/app.dart';
 
 void main() {
-  runApp(const test());
+  runApp(const footballder());
 }
--- flutter_application_1/lib/src/app.dart
+++ flutter_application_1/lib/src/app.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/src/bloc/login/login_bloc.dart';
 import 'package:flutter_application_1/src/pages/login/login.dart';
 import 'package:flutter_application_1/src/pages/routes.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class test extends StatelessWidget {
-  const test({super.key});
+class footballder extends StatelessWidget {
+  const footballder({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "Rit test", routes: AppRoute.all, home: LoginPage());
+    final loginBloc = BlocProvider(create: (context) => LoginBloc());
+
+    return MultiBlocProvider(
+        providers: [loginBloc],
+        child: MaterialApp(
+            title: "Rit test", routes: AppRoute.all, home: LoginPage()));
   }
 }
