1ecb3cf731218d84a4ca6fe1a982b34f694d0bfd
rittbpt <nutdanai.t@ku.th>
2024-03-28 14:47:28 +0700

    update

--- server/service/Request.js
+++ server/service/Request.js
@@ -27,7 +27,7 @@ const method = {
         }
     },
 
-    insert: async function (createTime, MatchId, Position, userId) {
+    insert: async function (MatchId, Position, userId) {
         try {
             const createtime = await dateHelper.DateNow()
             const sql = `INSERT INTO Request (createTime , MatchId , Position, statusRequest , userId) VALUES ('${createtime}' , '${MatchId}' , '${Position}' , 'wait' , ${userId})`;
